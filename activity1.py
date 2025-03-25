d64e4c558f4b392a99974e9bc6213aaac1a250bd
Ayara <sushma.paudel4317@gmail.com>
2025-03-24 18:51:00 -0600

    first commit

---
+++ activity1.py
@@ -0,0 +1,6 @@
+x=(input("Enter a word:")).upper()
+for i in x:
+    if i=="A":
+        print("A is found")
+    else:
+        print("A is not found")
---
+++ activity3.py
@@ -0,0 +1,7 @@
+x=10
+while x>0:
+    x=x-1 
+    if x==5:
+        continue
+    print("The current variable value is",x)
+print("goodbye!")
